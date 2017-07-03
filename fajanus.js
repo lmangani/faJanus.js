af64620788d62e6ba5c034778e1bbb61d362e440
Lorenzo <lorenzo.mangani@gmail.com>
2017-07-03 17:55:09 -0400

    fix up timers

--- fajanus.js
+++ fajanus.js
@@ -1,3 +1,5 @@
+/* FaJanus 1.0.0 */
+
 var janus = require('./janus_seq.js');
 var request = require('request');
 
@@ -6,28 +8,26 @@ var headersOpt = {
     "content-type": "application/json",
 };
 
-function sleep(time, callback) {
-	console.log('wait '+time)
-    var stop = new Date().getTime();
-    while(new Date().getTime() < stop + time) {
-        ;
-    }
-    callback();
-}
+console.log('... Fly, Fajanus!');
 
 var prevts = 0;
-janus.seq.forEach(function(data){
-	if (prevts == 0) { var wait = 0; }
-	else { var wait = parseInt((data.timestamp - prevts)/1000); }
+var wait = 0;
+var tot = janus.seq.length;
 
-	sleep(wait, function(){
-		//console.log(data.timestamp,wait);
-		request({ uri: target_host, method:'POST',json: JSON.stringify(data) }, function (error, response, body) {
-		  console.log(error,response);
+janus.seq.forEach(function(data,i){
+	if (prevts == 0) { wait += 0; }
+	else { wait += parseInt((data.timestamp - prevts)/1000); }
+
+	prevts = data.timestamp;
+	// data.timestamp = new Date().getTime();
+
+	setTimeout(function(data,i){
+		request({ uri: target_host, method:'POST',json: data }, function (error, response, body) {
 		  if (!error && response.statusCode == 200) {
-		    console.log(body.id) // Print the shortened url.
+		    // console.log(body.id)
 		  }
 		});
-		prevts = data.timestamp;
-	});
+	}, wait, data, i);
 });
+
+console.log();
