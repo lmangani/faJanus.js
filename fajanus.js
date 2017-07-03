90195dc9b2f5807e429ef19cf95c3d59c000adea
Lorenzo <lorenzo.mangani@gmail.com>
2017-07-03 18:09:03 -0400

    patch ts to processing

--- fajanus.js
+++ fajanus.js
@@ -14,20 +14,21 @@ var prevts = 0;
 var wait = 0;
 var tot = janus.seq.length;
 
-janus.seq.forEach(function(data,i){
+janus.seq.forEach(function(data){
 	if (prevts == 0) { wait += 0; }
 	else { wait += parseInt((data.timestamp - prevts)/1000); }
 
 	prevts = data.timestamp;
-	// data.timestamp = new Date().getTime();
 
-	setTimeout(function(data,i){
+	setTimeout(function(data){
+		var now = new Date();
+		data['@timestamp'] = now.toISOString();
 		request({ uri: target_host, method:'POST',json: data }, function (error, response, body) {
 		  if (!error && response.statusCode == 200) {
 		    // console.log(body.id)
 		  }
 		});
-	}, wait, data, i);
+	}, wait, data);
 });
 
 console.log();
